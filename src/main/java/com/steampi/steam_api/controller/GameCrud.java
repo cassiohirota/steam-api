a4574d7033df835b0758ef7e009ca309592cf498
cassiohirota <cassiohirota@gmail.com>
2024-10-31 14:19:20 -0300

    crud

--- src/main/java/com/steampi/steam_api/controller/GameCrud.java
+++ src/main/java/com/steampi/steam_api/controller/GameCrud.java
@@ -2,7 +2,10 @@ package com.steampi.steam_api.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,13 +17,35 @@ import com.steampi.steam_api.model.Genre;
 @Controller
 public class GameCrud {
 
+	@GetMapping(value = "/{id}")
+	@ResponseBody
+    public ResponseEntity<Game> getGame(@RequestBody Game game) {
+        System.out.println(game);
+        
+        return ResponseEntity.ok(game);
+    }
+	
 	@PostMapping(produces = "application/json")
 	@ResponseBody
     public ResponseEntity<Game> postGame(@RequestBody Game game) {
         System.out.println(game);
         
+        return ResponseEntity.ok(game);
+    }
 
-        // Retorna uma resposta indicando que o jogo foi processado com sucesso
+	@PutMapping(value = "/{id}")
+	@ResponseBody
+	public ResponseEntity<Game> putGame(@RequestBody Game game) {
+        System.out.println(game);
+        
+        return ResponseEntity.ok(game);
+    }
+
+	@DeleteMapping(value = "/{id}")
+	@ResponseBody
+	public ResponseEntity<Game> deleteGame(@RequestBody Game game) {
+        System.out.println(game);
+        
         return ResponseEntity.ok(game);
     }
 }
