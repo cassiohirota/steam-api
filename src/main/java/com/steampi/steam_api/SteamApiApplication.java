d16cf95cd653490ad0e1cf5de41a3057c3125c96
cassiohirota <cassiohirota@gmail.com>
2024-10-31 10:45:14 -0300

    Primeiro commit

---
+++ src/main/java/com/steampi/steam_api/SteamApiApplication.java
@@ -0,0 +1,13 @@
+package com.steampi.steam_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteamApiApplication.class, args);
+	}
+
+}
